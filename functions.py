5c8d02dbca05e120f583cd23606e33d78c728f9a
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2023-02-12 10:50:21 +0100

    done with move function

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import os
 def hsl_to_ansi(h, s, l, str,rgb=False):
     h /= 360
     s /= 100
@@ -68,3 +69,18 @@ def get_white_index(dico):
         for j in range(len(list(dico.values())[i])):
             if list(dico.values())[i][j]=='rgb(255,255,255)':
                 return (i,j)
+
+def move(i2,j2,dico):
+    l=list(dico.keys())
+    i,j=get_white_index(dico)
+    if d_between_indexes(l[i],l[i2],l)==1:
+        print(dico[list(dico.keys())[i]])
+        dico[list(dico.keys())[i]][j]=dico[list(dico.keys())[i2]][j2]
+        dico[list(dico.keys())[i2]][j2]="rgb(255,255,255)"
+    else:
+        print(to_rgb(255,0,0,"Impossible d'effectuer ce mouvement, non-adjacent."))
+    print(dico)
+
+
+print(to_rgb(255,0,0,"This file is not supposed to be run. You shoud run the file `main.py`"))
+#os.system("/usr/local/bin/python3 /Users/arl/prog/nsi/tri/main.py")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 from functions import *
+from random import choice
 with open("data.html") as f:
     content=f.readlines()
 d={}
@@ -10,24 +11,37 @@ for row in content:
         color_fill1=re.search('''"hsl(.*)"''',content[i]).group(1)
         color_fill=color_fill1.replace("(",'').replace(')','').replace("%",'').split(',')
         color_fill=[int(a) for a in color_fill]
-        d[hsl_to_ansi(color_fill[0],color_fill[1],color_fill[2],color_fill1)]=result
+        color_name="rgb"+str(hsl_to_ansi(color_fill[0],color_fill[1],color_fill[2],color_fill1,True))
+        color_fill=hsl_to_ansi(color_fill[0],color_fill[1],color_fill[2],color_fill1,True)
+        d[to_rgb(color_fill[0],color_fill[2],color_fill[1],color_name)]=result
 print(to_rgb(255,0,0,"OUTPUT >>>>>"))
 l=list(d.keys())
 print(d)
 print(show(d))
 truth=[False for i in range(len(d.keys()))]
+print(show(d))
 actual_base=0
 shots=0
+print(show(d))
+move(1,0,d)
+print(show(d))
+'''
 while prod(truth)!=True:
     #trouver les boules pouvant etre bougees
-    for a in l:
-      if d_between_indexes(l[get_white_index(d)[0]],a,list(d.keys()))==1:
-        print(a,"can be moved.")
+    for i in range(len(l)):
+      if d_between_indexes(l[get_white_index(d)[0]],l[i],l)==1:
+        if l[i-1] in d[l[i]]:
+            print(i,i-1,"TO BE MOVED UPPER")
+        elif l[i]==d[l[i-1]]:
+            print("TO BE MOVED TOO")
+        else:
+            move()
     #compteur de coups
     shots+=1
     #verification at each iteration
     for e in d.keys():
         if all_elements_equal(e):
             truth[l.index(e)]=1
-    print(show(d))
+    #print(show(d))
 print("En",shots,"coups!")
+'''
